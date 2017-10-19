cdfb0967ff7b4654a48054f8445987333bf8695e
Aditya Panchal <apanchal@bastula.org>
2017-10-19 10:56:07 -0500

    Add support for string/byte streams.

--- dicompylercore/dicomparser.py
+++ dicompylercore/dicomparser.py
@@ -19,7 +19,7 @@ except ImportError:
     from dicom.dataset import Dataset
 import random
 from numbers import Number
-from six import PY2, iterkeys, string_types
+from six import PY2, iterkeys, string_types, BytesIO
 from six.moves import range
 from dicompylercore import dvh, util
 
@@ -38,7 +38,7 @@ class DicomParser:
 
         if isinstance(dataset, Dataset):
             self.ds = dataset
-        elif isinstance(dataset, string_types):
+        elif isinstance(dataset, (string_types, BytesIO)):
             try:
                 self.ds = \
                     read_file(dataset, defer_size=100, force=True)
