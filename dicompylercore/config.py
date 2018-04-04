baeca40c725c47afb449263ee021a64c24e393a3
Aditya Panchal <apanchal@bastula.org>
2018-04-04 11:19:57 -0500

    Fix config to remove Codacy issues: https://app.codacy.com/app/bastula/dicompyler-core/pullRequest?prid=1505303

--- dicompylercore/config.py
+++ dicompylercore/config.py
@@ -10,15 +10,21 @@
 pil_available = True
 try:
     from PIL import Image
-except:
+except ImportError:
     pil_available = False
+else:
+    Image
 shapely_available = True
 try:
     from shapely.geometry import Polygon
-except:
+except ImportError:
     shapely_available = False
+else:
+    Polygon
 skimage_available = True
 try:
     from skimage.transform import rescale
 except ImportError:
     skimage_available = False
+else:
+    rescale
