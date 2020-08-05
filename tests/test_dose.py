7a3d77ae987afc29764c4aa17e932402cdd7efb5
cutright <dan.cutright@gmail.com>
2020-08-05 16:03:31 -0500

    Tests: add, mul, & rmul overload, add with attr mismatch. fixed interp testing

--- tests/test_dose.py
+++ tests/test_dose.py
@@ -16,11 +16,25 @@ try:
 except ImportError:
     from dicom.dataset import Dataset
     from dicom.sequence import Sequence
-from numpy import arange, zeros
-from numpy.testing import assert_array_almost_equal, assert_array_equal
+from numpy import arange
+from numpy.testing import (
+    assert_array_almost_equal,
+    assert_array_equal,
+    assert_raises,
+)
 
 basedata_dir = "tests/testdata"
 example_data = os.path.join(basedata_dir, "example_data")
+
+
+def assert_array_not_equal(arr_1, arr_2):
+    return assert_raises(AssertionError, assert_array_equal, arr_1, arr_2)
+
+
+def assert_array_not_almost_equal(arr_1, arr_2):
+    return assert_raises(
+        AssertionError, assert_array_almost_equal, arr_1, arr_2
+    )
 
 
 class TestDose(unittest.TestCase):
@@ -57,7 +71,7 @@ class TestDose(unittest.TestCase):
         # y-dimension
         self.assertEqual(scale[1], 2.5)
         # z-dimension
-        self.assertEqual(scale[2], 3.)
+        self.assertEqual(scale[2], 3.0)
 
     def test_offset(self):
         """Test dose grid resolution extraction from the DICOM data."""
@@ -86,33 +100,71 @@ class TestDose(unittest.TestCase):
         self.assertEqual(self.dosegrid.is_coincident(other), False)
 
         # GridFrameOffsetVector
+        check = arange(0, 98) * 3
+        assert_array_equal(self.dosegrid.ds.GridFrameOffsetVector, check)
         other = dose.DoseGrid(self.rtdose_dcm)
         other.ds.GridFrameOffsetVector[0] += 1
         self.assertEqual(self.dosegrid.is_coincident(other), False)
 
-        # GridFrameOffsetVector
-        check = arange(0, 98) * 3
-        assert_array_equal(self.dosegrid.ds.GridFrameOffsetVector, check)
+    def test_add_overload(self):
+        # Overloaded __add__ operator
+        other = dose.DoseGrid(self.rtdose_dcm)
+        dose_sum = self.dosegrid + other
+        assert_array_equal(self.dosegrid.dose_grid, other.dose_grid)
+        assert_array_equal(dose_sum.dose_grid, self.dosegrid.dose_grid * 2)
+
+    def test_multiply_overload(self):
+        # Overloaded __mul__ operator
+        scaled_dosegrid = self.dosegrid * 2
+        assert_array_equal(
+            scaled_dosegrid.dose_grid, self.dosegrid.dose_grid * 2
+        )
+
+    def test_right_multiply_overload(self):
+        # Overloaded __rmul__ operator
+        scaled_dosegrid = 2 * self.dosegrid
+        assert_array_equal(
+            scaled_dosegrid.dose_grid, self.dosegrid.dose_grid * 2
+        )
 
     def test_dose_direct_sum(self):
         # Direct Sum with a factor
-        other = dose.DoseGrid(self.rtdose_dcm) * 2
+        other = dose.DoseGrid(self.rtdose_dcm)
         other.direct_sum(self.dosegrid)
-        assert_array_equal(other.dose_grid, self.dosegrid.dose_grid * 3)
+        assert_array_equal(other.dose_grid, self.dosegrid.dose_grid * 2)
 
     def test_dose_interp_sum(self):
         # Interp Sum with a factor
-        other = dose.DoseGrid(self.rtdose_dcm) * 2
+        other = dose.DoseGrid(self.rtdose_dcm)
         other.interp_sum(self.dosegrid)
-        assert_array_almost_equal(other.dose_grid, self.dosegrid.dose_grid * 3)
+        assert_array_almost_equal(other.dose_grid, self.dosegrid.dose_grid * 2)
 
     def test_interp_entire_grid(self):
-        # Interp Sum, entire grid in one operation
+        # Interp Sum equality, entire grid in one operation
         other = dose.DoseGrid(self.rtdose_dcm)
-        other.interp_entire_grid(self.dosegrid)
-        assert_array_almost_equal(other.dose_grid, self.dosegrid.dose_grid)
+        other.x_axis += 0.0000005  # perturb to ensure interpolation is used
+        interp_grid_1 = other.interp_entire_grid(self.dosegrid)
+        assert_array_not_equal(interp_grid_1, self.dosegrid.dose_grid)
+        assert_array_almost_equal(interp_grid_1, self.dosegrid.dose_grid)
+
+        # Interp Sum inequality, entire grid in one operation
+        other.x_axis += 0.0000005  # should cause assert_array_equal failure
+        interp_grid_2 = other.interp_entire_grid(self.dosegrid)
+        assert_array_not_equal(interp_grid_2, self.dosegrid.dose_grid)
+        assert_array_not_almost_equal(interp_grid_2, self.dosegrid.dose_grid)
+
+    def test_add_attr_mismatch(self):
+        other = dose.DoseGrid(self.rtdose_dcm)
+        other.ds.DoseSummationType = ""
+        failure_detected = False
+        try:
+            other.add(self.dosegrid)
+        except NotImplementedError:
+            failure_detected = True
+        self.assertEqual(failure_detected, True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     sys.exit(unittest.main())
