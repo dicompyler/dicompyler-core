de6a0ed7f3a928399f799bce0acb6018e595a8bf
Dan Cutright <dan.cutright@gmail.com>
2021-04-02 09:10:00 -0500

    Fix Pixel data attr missing if value not set (#214)
    Remove the PixelData attribute if it is not set.

--- dicompylercore/dicomparser.py
+++ dicompylercore/dicomparser.py
@@ -58,6 +58,11 @@ class DicomParser:
                     raise AttributeError
         else:
             raise AttributeError
+        # Remove the PixelData attribute if it is not set.
+        # i.e. RTStruct does not contain PixelData and its presence can confuse
+        # the parser
+        if "PixelData" in self.ds and self.ds.PixelData is None:
+            delattr(self.ds, 'PixelData')
         if memmap_pixel_array:
             self.filename = dataset
             self.pixel_array = self.get_pixel_array
